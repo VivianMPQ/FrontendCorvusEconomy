2acdf65940f115896a3eb53ade12a368270d5313
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-21 18:32:15 -0500

    feat: added routes

---
+++ src/app/app-routing.modules.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import {SignInComponent} from "./security/pages/sign-in/sign-in.component";
+import {SignUpComponent} from "./security/pages/sign-up/sign-up.component";
+import {ResetPasswordComponent} from "./security/pages/reset-password/reset-password.component";
+import {ChangePasswordComponent} from "./security/pages/change-password/change-password.component";
+import {InputComponent} from "./public/input/input.component";
+import {OutputComponent} from "./public/output/output.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "sing-in", pathMatch: "full" },
+  { path: "sign-in", component: SignInComponent },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "reset-password", component: ResetPasswordComponent },
+  { path: "change-password", component: ChangePasswordComponent },
+  { path: "input", component: InputComponent },
+  { path: "output", component: OutputComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './security/pages/sign-in/sign-in.component';
+import { SignUpComponent } from './security/pages/sign-up/sign-up.component';
+import { ResetPasswordComponent } from './security/pages/reset-password/reset-password.component';
+import { ChangePasswordComponent } from './security/pages/change-password/change-password.component';
+import { InputComponent } from './public/input/input.component';
+import { OutputComponent } from './public/output/output.component';
+import {AppRoutingModule} from "./app-routing.modules";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ResetPasswordComponent,
+    ChangePasswordComponent,
+    InputComponent,
+    OutputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
